eafa22cf3b161a1294f7a78de6b30546c03ffb48
noahdude1203 <gissn@oregonstate.edu>
2022-11-21 23:55:31 -0800

    Attack UI
    Make data appear.

--- public/game.js
+++ public/game.js
@@ -1,14 +1,38 @@
 var compass = document.getElementById("compass")
-
+var content = "attacker"
+var attacker
+var defender
 
 document.querySelectorAll('.territory').forEach(item => {
     item.addEventListener('click', event => {
       var territory = event.currentTarget
       var territoryTroops = document.getElementById(territory.id + "-troops").textContent
+      var ownerRegion = document.getElementById(content+"-region")
+      var ownerTroops = document.getElementById(content+"-troops")
       console.log("Territory selected")
       console.log("--Name(ID):", territory.id)
+      ownerRegion.textContent = territory.id
       console.log("--Troops:", territoryTroops)
+      ownerTroops.textContent = territoryTroops
+      if (content === "attacker"){
+        attacker = event.currentTarget
+        content = "defender"
+      } else {
+        defender = event.currentTarget
+        content = "attacker"
+      }
     })
+})
+var attackButton = document.getElementById("attack-box")
+attackButton.addEventListener('click', function(){
+  console.log("Attack clicked")
+  var attackerTroops = document.getElementById("attacker-troops").textContent
+  var defenderTroops = document.getElementById("defender-troops").textContent
+  if (attackerTroops > 1 && defenderTroops > 0){
+    console.log("--Rolling Dice!")
+  }else{ 
+    console.log("--Not enough troops")
+  }
 })
 
 var sign = .25
@@ -25,3 +49,6 @@ window.onload = function() {
 }
 
 console.log(window.innerWidth, '+', window.innerWidth)
+
+
+//
