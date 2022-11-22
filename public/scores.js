d840b075f2d2d158d69b1d12541b5e48799a6c90
1Run <byron.ojua@gmail.com>
2022-11-21 16:12:18 -0800

    Added scores page and firebase

---
+++ public/scores.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD2Z8IyySBr5U2pNeDSoKPIH_UJ3L9nmcI",
+  authDomain: "benny-s-crusade.firebaseapp.com",
+  projectId: "benny-s-crusade",
+  storageBucket: "benny-s-crusade.appspot.com",
+  messagingSenderId: "890219837928",
+  appId: "1:890219837928:web:0df5749e10d70b7cb14c5d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
