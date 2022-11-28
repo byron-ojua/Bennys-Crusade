60f8e2e10fdfd5e102b9329d9e6c36de89fe924f
1Run <byron.ojua@gmail.com>
2022-11-27 21:13:39 -0800

    Incorperated favicon into server

--- server.js
+++ server.js
@@ -8,12 +8,16 @@ var fs = require("fs")
 var path = require("path")
 var express = require('express');
 var exphbs = require('express-handlebars');
+var favicon = require('serve-favicon')
 var port = 3000
 
 
 var app = express();
 
 app.use(express.static('public'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+// app.use(favicon('favicon.ico'));
+
 
 app.get('*', function (req, res) {
 	res.status(404).render('404', ({}))
