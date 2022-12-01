770d0957552b68610a8b7ea9cbc8a4933cfba325
1Run <byron.ojua@gmail.com>
2022-11-30 16:33:33 -0800

    Fixed attacking troop placement
    Co-authored-by: noahdude1203 <noahdude1203@users.noreply.github.com>

--- public/game.js
+++ public/game.js
@@ -22,193 +22,216 @@ shrinkMap.addEventListener('click', function() {
 
 //select territory
 document.querySelectorAll('.territory').forEach(item => {
-    item.addEventListener('click', event => {
-      clearTimeout(tooltipTimer)
-      hideDice()
-      var territory = event.currentTarget
-      var territoryTroops = document.getElementById(territory.id + "-troops").textContent
-      
-      console.log("Territory selected")
-      console.log("--Name(ID):", territory.id)
-      console.log("--Troops:", territoryTroops)
-      if (attacker != undefined){
-        neighbors = attacker.dataset.neighbor
-        console.log("--Neighbors:", neighbors)
-        console.log("--Neighbors.length:", neighbors.length)
+  item.addEventListener('click', event => {
+    clearTimeout(tooltipTimer)
+    hideDice()
+    var territory = event.currentTarget
+    var territoryTroops = document.getElementById(territory.id + "-troops").textContent
+    
+    console.log("Territory selected")
+    console.log("--Name(ID):", territory.id)
+    console.log("--Troops:", territoryTroops)
+    if (attacker != undefined){
+      neighbors = attacker.dataset.neighbor
+      console.log("--Neighbors:", neighbors)
+      console.log("--Neighbors.length:", neighbors.length)
 
-        if (neighbors.includes(territory.id)){
-          defender = territory
-          content = "defender"
-        }
+      if (neighbors.includes(territory.id)){
+        defender = territory
+        content = "defender"
       }
-      //set the correct info box to the right values.
-      updateAttackBox(territory, territoryTroops, content)
-
-      console.log("")
-      if (content == "attacker"){
-        attacker = territory
-        clearAttackBox("defender")
-      }
-      content = "attacker"
-      return
-    })
-
-    item.onmouseenter = function(e){
-      tooltipTimer = setTimeout(function() {
-        tooltip.style.top =  e.pageY + "px";
-        tooltip.style.left =  e.pageX + "px";
-        tooltip.style.display =  "block";
-
-        document.getElementById("tooltip-region").innerText = "Region: " + item.dataset.name
-        document.getElementById("tooltip-troops").innerText = "Troops: " + document.getElementById(item.id + "-troops").textContent
-        document.getElementById("tooltip-owner").innerText = "Owner: " + item.dataset.owner
-      }, 700)
     }
+    //set the correct info box to the right values.
+    updateAttackBox(territory, territoryTroops, content)
 
-    item.onmouseout = function() {
-      clearTimeout(tooltipTimer)
-      tooltip.style.display =  "none";
-
+    console.log("")
+    if (content == "attacker"){
+      attacker = territory
+      clearAttackBox("defender")
     }
-})
-function updateAttackBox(territory, territoryTroops, content){
-  var ownerRegion = document.getElementById(content+"-region")
-  var ownerTroops = document.getElementById(content+"-troops")
-  ownerRegion.textContent = territory.dataset.name
-  // ownerRegion.textContent = territory.id
-  ownerTroops.textContent = territoryTroops
-}
-function clearAttackBox(attackerOrDefender){
-  document.getElementById(attackerOrDefender+"-region").textContent = ""
-  document.getElementById(attackerOrDefender+"-troops").textContent = ""
-}
-function hideDice(){
-  diceBox = document.getElementById("dice-box")
-  allDice = diceBox.querySelectorAll(".dice")
-  for (var i = 0; i < allDice.length; i++){
-    allDice[i].style.display = "none"
+    content = "attacker"
+    return
+  })
+
+  item.onmouseenter = function(e){
+    tooltipTimer = setTimeout(function() {
+      tooltip.style.top =  e.pageY + "px";
+      tooltip.style.left =  e.pageX + "px";
+      tooltip.style.display =  "block";
+
+      document.getElementById("tooltip-region").innerText = "Region: " + item.dataset.name
+      document.getElementById("tooltip-troops").innerText = "Troops: " + document.getElementById(item.id + "-troops").textContent
+      document.getElementById("tooltip-owner").innerText = "Owner: " + item.dataset.owner
+    }, 700)
   }
-}
+
+  item.onmouseout = function() {
+    clearTimeout(tooltipTimer)
+    tooltip.style.display =  "none";
+
+  }
+})
+
+
 //Attack button when two countries are selected
+//Setting attack variables
+var troops
+var homeRegion
+var newRegion
 var attackButton = document.getElementById("attack-box")
 attackButton.addEventListener('click', function(){
-  hideDice()
-  console.log("Attack clicked")
-  var attackerTroops = document.getElementById("attacker-troops").textContent
-  var defenderTroops = document.getElementById("defender-troops").textContent
-  var attackDice = document.getElementById("attack-dice")
-  var defenseDice = document.getElementById("defense-dice")
-  
-
-  if (attackerTroops > 1 && defenderTroops > 0){
-    console.log("--Rolling Dice!")
-
-    //Roll Attack Dice
-    var diceRollAttack = []
-    var allAttackDice = attackDice.querySelectorAll(".dice")
-    console.log("Revealing All Attack Dice")
-    for (var i = 0; i < attackerTroops-1 && i <= 2; i++){
-      diceRollAttack.push((Math.floor(Math.random() * 6))+1)
-      allAttackDice[i].textContent = diceRollAttack[i]
-      allAttackDice[i].style.display = "flex"
-    }
-
-    //Roll Defense Dice
-    var diceRollDefense = [(Math.floor(Math.random() * 6))+1]
-    var allDefenseDice = defenseDice.querySelectorAll(".dice")
-    console.log("Revealing All Defense Dice")
-    allDefenseDice[0].textContent = diceRollDefense[0]
-    allDefenseDice[0].style.display = "flex"
-    if (defenderTroops > 1){
-      diceRollDefense.push((Math.floor(Math.random() * 6))+1)
-      allDefenseDice[1].textContent = diceRollDefense[1]
-      allDefenseDice[1].style.display = "flex"
-    }
+hideDice()
+console.log("Attack clicked")
+var attackerTroops = document.getElementById("attacker-troops").textContent
+var defenderTroops = document.getElementById("defender-troops").textContent
+var attackDice = document.getElementById("attack-dice")
+var defenseDice = document.getElementById("defense-dice")
 
 
-    diceRollAttack.sort().reverse()
-    diceRollDefense.sort().reverse()
-    console.log("All Dice", diceRollDefense, diceRollAttack)
+if (attackerTroops > 1 && defenderTroops > 0){
+  console.log("--Rolling Dice!")
 
-
-    //Resolve Troop losses
-    console.log("Resolving Troop losses")
-    for (var i = 0; i < diceRollAttack.length && i < diceRollDefense.length; i++){
-      if (diceRollAttack[i] > diceRollDefense[i]){
-        document.getElementById(defender.id + "-troops").textContent -= 1    
-        defenderTroops = document.getElementById(defender.id + "-troops").textContent
-        console.log(">>>>Lost an Defender")    
-      } else {
-        document.getElementById(attacker.id + "-troops").textContent -= 1
-        attackerTroops = document.getElementById(attacker.id + "-troops").textContent
-        console.log(">>>>Lost a Attacker")    
-      }
-    }
-
-  }else{ 
-    console.log("--Not enough troops")
+  //Roll Attack Dice
+  var diceRollAttack = []
+  var allAttackDice = attackDice.querySelectorAll(".dice")
+  console.log("Revealing All Attack Dice")
+  for (var i = 0; i < attackerTroops-1 && i <= 2; i++){
+    diceRollAttack.push((Math.floor(Math.random() * 6))+1)
+    allAttackDice[i].textContent = diceRollAttack[i]
+    allAttackDice[i].style.display = "flex"
   }
-  if (defenderTroops == 0){
-    sendTroops(attackerTroops, attacker.id, defender.id)
+
+  //Roll Defense Dice
+  var diceRollDefense = [(Math.floor(Math.random() * 6))+1]
+  var allDefenseDice = defenseDice.querySelectorAll(".dice")
+  console.log("Revealing All Defense Dice")
+  allDefenseDice[0].textContent = diceRollDefense[0]
+  allDefenseDice[0].style.display = "flex"
+  if (defenderTroops > 1){
+    diceRollDefense.push((Math.floor(Math.random() * 6))+1)
+    allDefenseDice[1].textContent = diceRollDefense[1]
+    allDefenseDice[1].style.display = "flex"
   }
-  updateAttackBox(attacker, attackerTroops, "attacker")
-  updateAttackBox(defender, defenderTroops, "defender")
+
+
+  diceRollAttack.sort().reverse()
+  diceRollDefense.sort().reverse()
+  console.log("All Dice", diceRollDefense, diceRollAttack)
+
+
+  //Resolve Troop losses
+  console.log("Resolving Troop losses")
+  for (var i = 0; i < diceRollAttack.length && i < diceRollDefense.length; i++){
+    if (diceRollAttack[i] > diceRollDefense[i]){
+      document.getElementById(defender.id + "-troops").textContent -= 1    
+      defenderTroops = document.getElementById(defender.id + "-troops").textContent
+      console.log(">>>>Lost an Defender")    
+    } else {
+      document.getElementById(attacker.id + "-troops").textContent -= 1
+      attackerTroops = document.getElementById(attacker.id + "-troops").textContent
+      console.log(">>>>Lost a Attacker")    
+    }
+  }
+
+}else{ 
+  console.log("--Not enough troops")
+}
+if (defenderTroops == 0){
+  troops = attackerTroops
+  homeRegion = attacker.id
+  newRegion = defender.id
+  sendTroops()
+}
+updateAttackBox(attacker, attackerTroops, "attacker")
+updateAttackBox(defender, defenderTroops, "defender")
 })
 
+//Setting send troops variables
+var homeTroops = document.getElementById("home-region")
+var newTroops = document.getElementById("new-region")
+var countryName = document.getElementById("country-names")
+function sendTroops(){
+console.log("Conquered New Territory!")
+console.log("--Total troops:", troops)
+console.log("--Home Region:", homeRegion)
+console.log("--New Region:", newRegion)
 
-function sendTroops(troops, homeRegion, newRegion){
-  console.log("Conquered New Territory!")
-  console.log("--Total troops:", troops)
-  console.log("--Home Region:", homeRegion)
-  console.log("--New Region:", newRegion)
-  
-  hideDice()
-  
-  showConquerBox()
+hideDice()
 
-  var homeTroops = document.getElementById("home-region")
-  console.log("homeTroops", homeTroops)
-  homeTroops.textContent = troops-1
+showConquerBox()
 
-  var newTroops = document.getElementById("new-region")
-  newTroops.textContent = 1
+console.log("homeTroops", homeTroops)
+homeTroops.textContent = troops-1
 
-  var countryName = document.getElementById("country-names")
-  countryName.textContent = homeRegion + "  >>>  " + newRegion
 
-  var newButton = document.getElementById("new-button")
-  newButton.addEventListener('click', function(){
-    if (homeTroops.textContent <= 1){return}
-    newTroops.textContent = parseInt(newTroops.textContent) + 1
-    homeTroops.textContent = parseInt(homeTroops.textContent) - 1
-  })
+newTroops.textContent = 1
 
-  var homeButton = document.getElementById("home-button")
-  homeButton.addEventListener('click', function(){
-    if (newTroops.textContent <= 1){return}
-    newTroops.textContent = parseInt(newTroops.textContent) - 1
-    homeTroops.textContent = parseInt(homeTroops.textContent) + 1
-  })
+countryName.textContent = homeRegion + "  >>>  " + newRegion
+homeButton.removeEventListener()
+}
 
-  var confirm = document.getElementById("conquer-done")
-  confirm.addEventListener('click', function(){
-    document.getElementById(homeRegion + "-troops").textContent = homeTroops.textContent
-    document.getElementById(newRegion + "-troops").textContent = newTroops.textContent
-    clearAttackBox("attacker")
-    clearAttackBox("defender")
-    content = "attacker"
-    attacker = undefined
-    defender = undefined
-    hideConquerBox()
-  })
+
+//Conquer Menu Buttons
+var homeButton = document.getElementById("home-button")
+homeButton.addEventListener('click', addHomeTroops)
+var newButton = document.getElementById("new-button")
+newButton.addEventListener('click', addNewTroops)
+var confirmConquer = document.getElementById("conquer-done")
+confirmConquer.addEventListener('click', confirmAddTroops)
+
+function addHomeTroops(){
+console.log("Adding troops to home territory")
+if (newTroops.textContent <= 1){return}
+newTroops.textContent = parseInt(newTroops.textContent) - 1
+homeTroops.textContent = parseInt(homeTroops.textContent) + 1
+}
+function addNewTroops(){
+console.log("Adding troops to new territory")
+if (homeTroops.textContent <= 1){return}
+newTroops.textContent = parseInt(newTroops.textContent) + 1
+homeTroops.textContent = parseInt(homeTroops.textContent) - 1
+}
+function confirmAddTroops(){
+console.log("Confirming troops placement")
+
+
+document.getElementById(homeRegion + "-troops").textContent = homeTroops.textContent
+document.getElementById(newRegion + "-troops").textContent = newTroops.textContent
+content = "attacker"
+attacker = undefined
+defender = undefined
+clearAttackBox("attacker")
+clearAttackBox("defender")
+
+hideConquerBox()
+}
+function hideDice(){
+console.log("hiding dice")
+diceBox = document.getElementById("dice-box")
+allDice = diceBox.querySelectorAll(".dice")
+for (var i = 0; i < allDice.length; i++){
+  allDice[i].style.display = "none"
+}
 }
 function showConquerBox(){
-  document.getElementById("conquer-box").style.display = "flex"
-  document.getElementById("conquer-backdrop").style.display = "block"
+console.log("showingConquerBox")
+document.getElementById("conquer-box").style.display = "flex"
+document.getElementById("conquer-backdrop").style.display = "block"
 }
 function hideConquerBox(){
-  document.getElementById("conquer-box").style.display = "none"
-  document.getElementById("conquer-backdrop").style.display = "none"
+document.getElementById("conquer-box").style.display = "none"
+document.getElementById("conquer-backdrop").style.display = "none"
+}
+function updateAttackBox(territory, territoryTroops, content){
+var ownerRegion = document.getElementById(content+"-region")
+var ownerTroops = document.getElementById(content+"-troops")
+ownerRegion.textContent = territory.dataset.name
+// ownerRegion.textContent = territory.id
+ownerTroops.textContent = territoryTroops
+}
+function clearAttackBox(attackerOrDefender){
+document.getElementById(attackerOrDefender+"-region").textContent = ""
+document.getElementById(attackerOrDefender+"-troops").textContent = ""
 }
 
 var sign = .25
@@ -222,6 +245,14 @@ window.onload = function() {
       sign = sign * -1
     }
   }, 100)
+  
+  var colors = localStorage.getItem("playerColors")
+  var colorsArray = JSON.parse(colors)
+  for(var i = 0; i < colorsArray.length; i++) {
+	  var id = i.toString()
+	  var sidebar = document.getElementById(id)
+	  sidebar.style.background = 'linear-gradient(to right, white 2%, ' + colorsArray[i] + ' 110%) left';
+  }
 }
 
 console.log(window.innerWidth, '+', window.innerWidth)
